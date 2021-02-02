233c911af1845068b08f42234caeea5e079c48d9
unknown <he.sha@husky.neu.edu>
2021-02-02 13:05:15 -0500

    added node server to back end

--- grocery-node-be/server.js
+++ grocery-node-be/server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin",
+        "http://localhost:4200");
+    res.header("Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods",
+        "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+});
+
 app.get('/', function(req, res) {
     res.send("hello world").then(console.log("server started!"));
 });
+
 app.listen(3000);
